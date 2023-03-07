6e9c7029d05301241e038d217872c5a2ea854d99
Abhijit Malvadkar <abhijit@sorigin.com>
2023-03-07 23:01:30 +0530

    Got the scrol working

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,26 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  id: any;
   selectedIndex: any;
-  navMenu = [
-    {btnName: 'Home'},
-    {btnName: 'About'},
-    {btnName: 'Contact'}
-  ];
+  navMenu = [{ btnName: 'Home' }, { btnName: 'About' }, { btnName: 'Contact' }];
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  public selectedBtn(_index: number) {
+  public selectedBtn(_index: number, btnName: any) {
     this.selectedIndex = _index;
+    if (btnName === 'Home') {
+      this.id = 'landing-page';
+      this.scroll(this.id)
+    } else if (btnName === 'About') {
+      this.id = 'about-page';
+      this.scroll(this.id)
+    } else  if (btnName === 'Contact') {
+      this.id = 'contact-us-page';
+      this.scroll(this.id)
+    }
   }
 
+  scroll(id: any) {
+    console.log(`scrolling to ${id}`);
+    let el = document.getElementById(id);
+    // @ts-ignore: Object is possibly 'null'.
+    el.scrollIntoView({behavior:"smooth"});
+  }
 }
